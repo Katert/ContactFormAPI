f906ab7b618fb2166e93329acc451f964a7d81c6
Wesley Kater <wesley.kater@icloud.com>
2020-08-28 21:09:10 +0200

    minor fix for port listening on heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log("Listening..");
 });
 
