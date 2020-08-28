154744c6ead48cb91566c89d31f48ff94ed161d5
Wesley Kater <wesley.kater@icloud.com>
2020-08-28 20:57:25 +0200

    changed smtp settings

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
   console.log("Listening..");
 });
 
@@ -23,41 +23,37 @@ app.post("/api/v1", (req, res) => {
   var formData = req.body;
 
   var smtpTransport = nodemailer.createTransport({
+    host: "smtp.strato.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "contact@wesleykater.com",
+      pass: process.env.CONTACT_PW,
+    },
+  });
 
-		host: "smtp.sendgrid.net",
-		port: 465,
-		secure: true,
-		auth: {
-			user: 'apikey',
-			pass: process.env.SENDGRID_API_KEY
-		}
-
-	})
-
-	var mailOptions = {
-		from: 'wesk0601@gmail.com',
-		to: 'wesk0601@gmail.com',
-		subject: `${formData.fname} sent you a message!`,
-		html: `<p>First name: ${formData.fname}</p>\n
+  var mailOptions = {
+    from: "contact@wesleykater.com",
+    to: "wesk0601@gmail.com",
+    subject: `${formData.fname} sent you a message!`,
+    html: `<p>First name: ${formData.fname}</p>\n
 		<p>Last name: ${formData.lname}</p>\n
 		<p>Email: ${formData.email}</p>\n
-		<p>Subject: ${formData.subject}</p>\n
 		<p>Phone: ${formData.phone}</p>\n
-		<p>Company: ${formData.company}</p>\n
-		<p>Message:<br/><br/> ${formData.message}</p>`
-	}
+    <p>Company: ${formData.company}</p>\n\n
+    <p>Subject: ${formData.subject}</p>\n
+		<p>Message:<br/><br/> ${formData.message}</p>`,
+  };
 
-	smtpTransport.sendMail(mailOptions, (error, response) => {
-		
-		if (error) {
-			console.log(error)
-			res.send(error)
-		} else {
-			console.log(response)
-			res.send('Success')
-		}
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    } else {
+      console.log(response);
+      res.send("Success");
+    }
 
-		smtpTransport.close();
-	})
-
+    smtpTransport.close();
+  });
 });
